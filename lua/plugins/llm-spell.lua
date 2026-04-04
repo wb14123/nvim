0a146059e1aeb961a8cdf37f679675158321959e
Bin Wang <bin.wang@mail.binwang.me>
2026-04-04 19:58:56 -0400

    add nvim-llm-spell plugin

---
+++ lua/plugins/llm-spell.lua
@@ -0,0 +1,9 @@
+return {
+  -- dir = "/home/wangbin/hobbi_source/nvim-llm-spell",
+  "wb14123/nvim-llm-spell",
+  opts = {
+    api_url = "http://localhost:8080/v1/chat/completions",
+    api_key_env = "",
+    model = "unsloth/gemma-4-E4B-it-GGUF:UD-Q4_K_XL",
+  },
+}
