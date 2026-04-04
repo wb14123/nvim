e03cf97cea75b7c4d769a46981154381e44cd1a7
Bin Wang <bin.wang@mail.binwang.me>
2026-04-04 19:42:46 -0400

    fix highlight for mis-spell

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,11 @@
 return {
   -- add gruvbox
-  { "rebelot/kanagawa.nvim" },
+  {
+    "rebelot/kanagawa.nvim",
+    opts = {
+      undercurl = false, -- use underline for terminals without undercurl support
+    },
+  },
 
   -- Configure LazyVim to load gruvbox
   {
