081e387fbec760c74c63bfb6e35154a49ab3d1a5
Bin Wang <bin.wang@mail.binwang.me>
2026-04-06 18:34:08 -0400

    remove moving line keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Disable Alt+j/k move-line keymaps to prevent accidental line swaps
+-- (tmux can interpret ESC followed by j/k as Alt+j/k)
+vim.keymap.del({ "n", "i", "v" }, "<A-j>")
+vim.keymap.del({ "n", "i", "v" }, "<A-k>")
