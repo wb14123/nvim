91bbbd191337709fc3cd82dc138ea3d9ddcd6f0f
Bin Wang <bin.wang@mail.binwang.me>
2026-04-10 14:22:38 -0400

    render-markdown: use '#' markers instead of circled-number heading icons

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -4,6 +4,12 @@ return {
     ft = { "markdown", "tcode" },
     opts = {
       file_types = { "markdown", "tcode" },
+      heading = {
+        -- Show the original '#', '##', '###'... instead of the default
+        -- circled-number icons. Trailing space provides separation between
+        -- the marker and the heading title.
+        icons = { "# ", "## ", "### ", "#### ", "##### ", "###### " },
+      },
     },
   },
 }
