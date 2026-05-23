5fc7fbb055d06f966f31c45a546a5c21156518cf
Bin Wang <bin.wang@mail.binwang.me>
2026-05-23 11:55:50 -0400

    enable line wrap in diff

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,13 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Diff mode: enable line wrapping in all diff windows (matching ~/.vimrc behavior)
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = vim.api.nvim_create_augroup("diff_settings", { clear = true }),
+  callback = function()
+    if vim.opt.diff:get() then
+      vim.cmd("windo set wrap")
+    end
+  end,
+})
